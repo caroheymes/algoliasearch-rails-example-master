80a0a738a603314c92bdf0bc73560d46f6746a1c
Caroline Heymes <caroline.heymes@gmail.com>
2017-12-15 13:59:04 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,15 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require "active_model/railtie"
+require "active_job/railtie"
+
+require "action_controller/railtie"
+
+require "action_view/railtie"
+
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
